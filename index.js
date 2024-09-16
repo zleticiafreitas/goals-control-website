850200e28ac520d42e3223efe4756407b80eaa00
Leticia Freitas <leticiadev24@gmail.com>
2024-09-15 21:00:54 -0300

    starting app menu

---
+++ index.js
@@ -0,0 +1,39 @@
+const {select} = require('@inquirer/prompts')
+
+const start = async () => {
+    while(true){
+        const opcao = await select({
+            message: 'Menu >',
+            choices: [
+                {
+                    name: 'Cadastrar meta',
+                    value: 'cadastrar'
+                },
+                {
+                    name: 'Listar metas',
+                    value: 'listar'
+                },
+                {
+                    name: 'Sair',
+                    value: 'sair'
+                }
+            ]
+        })
+
+
+        switch(opcao){
+            case 'cadastrar':
+                console.log("vamos cadastrar")
+                break
+            case 'listar':
+                console.log("vamos listar")
+                break
+            case 'sair':
+                console.log("Até a próxima!")
+                return
+
+        }
+    }
+}
+
+start()
